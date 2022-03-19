ca5a595e33d4a26e758edc9246832c68ad23a55a
abubaker <abubakerdahab@gmail.com>
2022-03-19 13:00:56 +0200

    add startup to org the index.js file, and add error middleware to handle express error.

---
+++ startup/routes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const users = require("../routes/users");
+const teams = require("../routes/teams");
+const auth = require("../routes/auth");
+const projects = require("../routes/projects");
+const tasks = require("../routes/tasks");
+const error = require("../middleware/error");
+
+module.exports = function (app) {
+  app.use(express.json());
+  app.use("/api/v1/users", users);
+  app.use("/api/v1/projects", projects);
+  app.use("/api/v1/tasks", tasks);
+  app.use("/api/v1/teams", teams);
+  app.use("/api/v1/auth", auth);
+  app.use(error);
+};
